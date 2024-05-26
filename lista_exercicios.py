0a10a6e8444d2fd3630016f2ebe8078fc132b391
Rafael Leite <rafael.eng0786@outlook.com>
2024-05-26 15:19:42 -0300

    Primeiro Exercicio

---
+++ lista_exercicios.py
@@ -0,0 +1,11 @@
+# Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+primeiro_num = int(input("numero01: "))
+segundo_num = int(input("numero02: "))
+soma = primeiro_num + segundo_num
+print(f"Aqui está o resultado: {soma}")
+
+# Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
