9f8c8942821341e610ac36594e518739c3193d97
Rafael Leite <rafael.eng0786@outlook.com>
2024-05-26 15:30:40 -0300

    Terceiro Exercicio

--- lista_exercicios.py
+++ lista_exercicios.py
@@ -7,11 +7,17 @@
 
 # Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 
-numero_user = int(input("Digite o seu número: "))
-NUM_CONSTANTE = 5
-resto_div = numero_user % NUM_CONSTANTE
-print(f"Aqui está o resultado: {resto_div}")
+# numero_user = int(input("Digite o seu número: "))
+# NUM_CONSTANTE = 5
+# resto_div = numero_user % NUM_CONSTANTE
+# print(f"Aqui está o resultado: {resto_div}")
 
 # Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+numero01 = int(input("Número01: "))
+numero02 = int(input("Número02: "))
+mult = numero01 * numero02
+print(f"Esse é o resultado: {mult}")
+
 # Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
