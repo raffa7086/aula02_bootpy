c83be14330e79d3d9d226217b5d8e44eab4ac305
Rafael Leite <rafael.eng0786@outlook.com>
2024-05-26 15:27:02 -0300

    Segundo exercicio

--- lista_exercicios.py
+++ lista_exercicios.py
@@ -1,11 +1,17 @@
 # Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 
-primeiro_num = int(input("numero01: "))
-segundo_num = int(input("numero02: "))
-soma = primeiro_num + segundo_num
-print(f"Aqui está o resultado: {soma}")
+# primeiro_num = int(input("numero01: "))
+# segundo_num = int(input("numero02: "))
+# soma = primeiro_num + segundo_num
+# print(f"Aqui está o resultado: {soma}")
 
 # Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+numero_user = int(input("Digite o seu número: "))
+NUM_CONSTANTE = 5
+resto_div = numero_user % NUM_CONSTANTE
+print(f"Aqui está o resultado: {resto_div}")
+
 # Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
