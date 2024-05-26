baf544af7def45dfa295ce9f01af14931e9f7ced
Rafael Leite <rafael.eng0786@outlook.com>
2024-05-26 10:43:03 -0300

    Segundo exercício

--- exercicios.py
+++ exercicios.py
@@ -1,5 +1,6 @@
-# Programa que soma dois números inseridos pelo usuário
-numero01 = int(input("número01: "))
-numero02 = int(input("número02: "))
-soma = numero01 + numero02
-print(soma)
+# Programa que recebe dois números inteiros e imprime a divisão inteira do primeiro pelo segundo
+
+numero01 = int(input("Digite o primeiro número inteiro: "))
+numero02 = int(input("Digite o segundo número inteiro: "))
+resultado = numero01 // numero02
+print(resultado)
