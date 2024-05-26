09c98934a8101dc93c39084f099ae7fb462ed887
Rafael Leite <rafael.eng0786@outlook.com>
2024-05-26 10:56:50 -0300

    Calcular a area do circulo

--- exercicios.py
+++ exercicios.py
@@ -1,6 +1,14 @@
+import math
 # Programa que recebe dois números inteiros e imprime a divisão inteira do primeiro pelo segundo
 
-numero01 = int(input("Digite o primeiro número inteiro: "))
-numero02 = int(input("Digite o segundo número inteiro: "))
-resultado = numero01 // numero02
-print(resultado)
+# numero01 = int(input("Digite o primeiro número inteiro: "))
+# numero02 = int(input("Digite o segundo número inteiro: "))
+# resultado = numero01 // numero02
+# print(resultado)
+
+# Exercicio 2 (Calcular a área do círculo)
+
+
+raio_circulo = float(input("Digite o raio do círulo: "))
+area_circulo = math.pi * raio_circulo ** 2
+print(area_circulo)
