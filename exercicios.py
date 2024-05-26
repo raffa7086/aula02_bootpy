a601b6adf4816ff108adce9792246015e2e19af5
Rafael Leite <rafael.eng0786@outlook.com>
2024-05-26 11:19:06 -0300

    Programa extração de valores

--- exercicios.py
+++ exercicios.py
@@ -9,6 +9,15 @@ import math
 # Exercicio 2 (Calcular a área do círculo)
 
 
-raio_circulo = float(input("Digite o raio do círulo: "))
-area_circulo = math.pi * raio_circulo ** 2
-print(f"{area_circulo:.2f}")
+# raio_circulo = float(input("Digite o raio do círulo: "))
+# area_circulo = math.pi * raio_circulo ** 2
+# print(f"{area_circulo:.2f}")
+
+# * Novo Exercicio: Crie um programa que peça ao usuário para digitar uma data no formato dd/mm/aaaa e em seguida imprima os valores dessa data separadamente
+
+data_usuario = input("Digite a data no formato dd/mm/aaaa: ")
+valores_lista_separado = data_usuario.split("/")
+print(f"O primeiro valor é o: {valores_lista_separado[0]}")
+print(f"O segundo valor é o: {valores_lista_separado[1]}")
+print(f"O terceiro valor é o: {valores_lista_separado[2]}")
+
